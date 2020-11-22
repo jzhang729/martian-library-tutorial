ed5235df03135c06b3164d9661b76e7f16578003
Jordan Zhang <jordanzhang@gmail.com>
2020-11-22 17:11:28 -0500

    Add and Update Item mutations.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,11 +3,6 @@ module Types
     field :id, ID, null: false
     field :email, String, null: true
     field :full_name, String, null: false
-    # field :first_name, String, null: true
-    # field :last_name, String, null: true
-    
-    # field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    # field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
     def full_name
       # `object` references the user instance
